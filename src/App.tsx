c09421ca6da0b7ad63562328ead61ade7edc2bb0
suraj.be <suraj.be@media.net>
2024-05-14 15:48:21 +0530

    create Place type inside api folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Map from "./components/Map";
 import LocationSearch from "./components/LocationSearch";
+import type { Place } from "./api/Place";
 
 function App() {
   return (
-    <div className="h-screen w-screen grid grid-cols-12">
-      <div className="col-span-3 p-2">
+    <div className='h-screen w-screen grid grid-cols-12'>
+      <div className='col-span-3 p-2'>
         <LocationSearch />
       </div>
-      <div className="col-span-9">
+      <div className='col-span-9'>
         <Map />
       </div>
     </div>
