fe36d49ac68f607a994b55d0e568b2e1362e7d3f
suraj.be <suraj.be@media.net>
2024-05-14 14:24:40 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Map from "./components/Map";
+import LocationSearch from "./components/LocationSearch";
+
+function App() {
+  return (
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
+  );
+}
+
+export default App;
